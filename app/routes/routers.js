d599b5c75d830c711fd66229f2c27d88cf7fe0ee
VinteloBrechos <vintelobrechos@gmail.com>
2024-08-17 07:52:11 -0300

    atualização do front

--- app/routes/routers.js
+++ app/routes/routers.js
@@ -9,6 +9,10 @@ router.get('/artigo', function(req, res){
     res.render('pages/artigo');
 })
 
+router.get('/login', function(req, res){
+    res.render('pages/login');
+})
+
 
 
 module.exports = router;
