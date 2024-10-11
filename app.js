49fe73d82a9a2b57307bccc809493c414d649c2f
VinteloBrechos <vintelobrechos@gmail.com>
2024-10-11 05:27:20 -0300

    atuaçização

--- app.js
+++ app.js
@@ -23,3 +23,38 @@ console.log("teste")
 app.listen(port,() => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+//                                          carrossel dos artigos
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const carouselSlide = document.querySelector('.carousel-slide');
+const images = document.querySelectorAll('.carousel-slide img');
+
+let counter = 0;
+const size = images[0].clientWidth;
+
+//                                       Função para mover o carrossel
+function moveCarousel() {
+  carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+}
+
+//                                          Botão de próxima imagem
+nextButton.addEventListener('click', () => {
+  if (counter >= images.length - 1) return; 
+  counter++;
+  moveCarousel();
+});
+
+//                                             Botão de imagem anterior
+prevButton.addEventListener('click', () => {
+  if (counter <= 0) return; 
+  counter--;
+  moveCarousel();
+});
+
+//                                             Redimensionamento dinâmico 
+window.addEventListener('resize', () => {
+  size = images[0].clientWidth;
+  moveCarousel();
+});
