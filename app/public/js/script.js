c7b9cae2c17c52792ebaf8839a456c19cfb0b290
VinteloBrechos <vintelobrechos@gmail.com>
2024-10-19 08:31:32 -0300

    atualização

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -8,3 +8,25 @@ document.querySelector('.search-icon').addEventListener('click', function() {
         alert('Por favor, insira um termo para buscar.');
     }
   });
+
+
+
+
+  // Imagens na seçao de produtos
+var modal = document.getElementById("modal");
+var img = document.getElementById("imagem-clique");
+var modalImg = document.getElementById("imagem-modal");
+var captionText = document.getElementById("legenda");
+var fechar = document.getElementsByClassName("fechar")[0];
+
+// Quando o usuário clica na imagem, ela é ampliada no modal
+img.onclick = function() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt; // Exibe o alt como legenda
+}
+
+// Quando o usuário clica no "x", o modal fecha
+fechar.onclick = function() {
+  modal.style.display = "none";
+}
