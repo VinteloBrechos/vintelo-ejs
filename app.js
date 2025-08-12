2cfc08f14af2b869708b64ab6b190e38f1ae794b
VinteloBrechos <vintelobrechos@gmail.com>
2025-08-12 10:50:27 -0300

    atualização-app lavis

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-
 const express = require('express');
 const path = require('path');
 const env = require('dotenv').config();
+
+const app = express();
 
 var session = require('express-session');
 app.use(
   session({
     secret: "Vintelo",
     resave: false,
-    saveUninitializede: false,
-  }));
+    saveUninitialized: false, 
+  })
+);
 
-
-const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("./app/public"));
-
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'app/public')))
-
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 var rotas = require("./app/routes/routers");
 app.use("/", rotas);
 
-console.log("teste")
+console.log("teste");
 
-app.listen(process.env.PORT,() => {
-  console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
