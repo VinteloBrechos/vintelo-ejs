0faef79018be95f5dd1180e1fbb9dfd312748bd8
Vintélo <vintelobrechos@gmail.com>
2025-08-12 15:03:29 +0000

    middleware mari

--- app/models/autenticador_middleware.js
+++ app/models/autenticador_middleware.js
@@ -26,6 +26,44 @@ gravarUsuAutenticado = async (req, res, next) => {
         var dadosForm = {
             user_usuario: req.body.nome_usu,
             senha_usuario: req.body.senha_usu,
+        };
+        var results = await usuario.findUserEmail(dadosForm);
+        var total = Object.keys(results).length;
+        if(total ==1){
+            if (bcrypt.compareSync(dadosForm.senha_usuario, results[0].senha_usuario)) {
+                var autenticado = {
+                    autenticado: results[0].NOME_USUARIO,
+                    id: results[0].ID_USUARIO,
+                    tipo: results[0].TIPO_USUARIO, //COLOCAR NA TABELA USUARIO DO BANCO//
+                    img_perfil_banco: results[0].img_perfil_banco != null ? `data:image/jpeg;base64,${results[0].img_perfil_banco.toString('base64')}` : null,
+                    img_perfil_pasta: results[0].img_perfil_pasta,
+                };
+            }
         }
     }
-}
+    req.session.autenticado = autenticado;
+    req.session.logado = 0 
+    next();
+};
+
+verificarUsuAutorizado = (tipoPermitido, destinoFalha) => {
+    return(req, res, next) => {
+        if (
+            req.session.autenticado.autenticado != null &&
+            tipoPermitido.find(function (element) {
+                return element == req.session.autenticado.tipo;
+            }) !=undefined
+        ) {
+            next();
+        } else {
+            res.render(destinoFalha, req.session.autenticado);
+        }
+    };
+};
+
+module.exports = {
+    verificarUsuAutenticado,
+    limparSessao,
+    gravarUsuAutenticado,
+    verificarUsuAutenticado,
+};
