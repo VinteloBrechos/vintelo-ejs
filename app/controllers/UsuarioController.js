2ac8da42b77b5ed1868abdf78e9563f52d13b57a
Vintélo <vintelobrechos@gmail.com>
2025-08-14 12:09:26 +0000

    controller incompleto mari

--- app/controllers/UsuarioController.js
+++ app/controllers/UsuarioController.js
@@ -3,3 +3,14 @@ const { body, validationResult} = require("express-validator");
 const bcrypt = require("bcryptjs");
 var salt = bcrypt.genSaltSync(12);
 const {removeImg} = require("../util/removeImg");
+const fetch = (...args) =>('node-fetch').then(({default: fetch}) => fetch(...args));
+const https = require("https");
+
+const usuarioController = {
+    regrasValidacaoFormLogin: [
+        body("nome_usu")
+            .isLength({min: 8, max: 45})
+            .withMessage("O nome deve conter pelo menos 8 caracteres!"),
+        body("")
+    ]
+}
