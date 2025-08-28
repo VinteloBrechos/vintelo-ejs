a6d19323f5a008d544a15bc29d93c514c8938058
Vintélo <vintelobrechos@gmail.com>
2025-08-28 15:29:13 +0000

    mari model

--- app/models/usuarioModel.js
+++ app/models/usuarioModel.js
@@ -5,7 +5,7 @@ const usuarioModel = {
         try {
             const [resultados] = await pool.query(
                 "SELECT u.ID_USUARIO, u.NOME_USUARIO, u.USER_USUARIO, " + 
-                "u.SENHA_USUARII, u.EMAIL_USUARIO, u.CELULAR_USUARIO"
+                "u.SENHA_USUARII, u.EMAIL_USUARIO, u.CELULAR_USUARIO, U."
             )
         }
     }
