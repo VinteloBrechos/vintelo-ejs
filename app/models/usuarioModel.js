13d39069a5567d305b89aaf8eec49c9f085bf2f5
Vintélo <vintelobrechos@gmail.com>
2025-08-28 15:25:20 +0000

    MARI MODEL

--- app/models/usuarioModel.js
+++ app/models/usuarioModel.js
@@ -1,1 +1,12 @@
-    module.exports = usuarioModel
+var pool = require("../config/pool_conexoes");
+
+const usuarioModel = { 
+    findAll: async () => {
+        try {
+            const [resultados] = await pool.query(
+                "SELECT u.ID_USUARIO, u.NOME_USUARIO, u.USER_USUARIO, " + 
+                "u.SENHA_USUARII, u.EMAIL_USUARIO, u.CELULAR_USUARIO"
+            )
+        }
+    }
+}
