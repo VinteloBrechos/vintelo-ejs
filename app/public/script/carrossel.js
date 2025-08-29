e0959fa7be1b7c0dd77066cfb5b2e454f0abecfb
Vintélo <vintelobrechos@gmail.com>
2025-08-29 11:14:17 +0000

    ajuste

--- app/public/script/carrossel.js
+++ app/public/script/carrossel.js
@@ -12,15 +12,4 @@
 
 
     
-    // rolagem do carrossel de categoris da página home //
-    const container = document.querySelector('.carousel-container');
-    const list = document.querySelector('.carousel-list');
-    const itemWidth = list.scrollWidth / 2; // metade, pois duplicamos
-    
-    container.addEventListener('scroll', () => {
-      if (container.scrollLeft >= itemWidth) {
-        container.scrollLeft -= itemWidth; // volta pro início sem travar
-      } else if (container.scrollLeft <= 0) {
-        container.scrollLeft += itemWidth; // vai pro final se rolar pra trás
-      }
-    });
+
