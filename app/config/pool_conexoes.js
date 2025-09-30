a52418faf8fd85b8732af5a2e3deb0357c685b1e
vintelobrechos <vintelobrechos@gmail.com>
2025-09-30 13:44:52 +0000

    mari ajustes

--- app/config/pool_conexoes.js
+++ app/config/pool_conexoes.js
@@ -1,19 +1,22 @@
 const mysql = require('mysql2')
-try {
-    var pool = mysql.createConnection({
-        host: process.env.HOST,
-        user: process.env.DB_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        port: process.env.PORT,
-    });
-    console.log("Conexão estabelecida!");
-} catch (e) {
-    console.log("Falha ao estabelecer a conexão!");
-    console.log(e);
-}
- 
-module.exports = pool.promise();
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    
+});
 
- 
+pool.getConnection((err, conn) => {
+    if(err) 
+        console.log(err)
+    else
+        console.log("Conectado ao SGBD!")
+})
+
+module.exports = pool.promise()
